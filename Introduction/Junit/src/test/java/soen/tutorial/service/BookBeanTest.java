83f2c994a6d7e31311af16e9c1017ced0afcdba1
Fahimeh Hajari <fahime.hajari@gmail.com>
2021-09-16 11:47:59 -0400

    Introduction Junit sample

---
+++ Introduction/Junit/src/test/java/soen/tutorial/service/BookBeanTest.java
@@ -0,0 +1,36 @@
+package soen.tutorial.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.Test;
+
+import soen.tutorial.service.BookBean;
+import soen.tutorial.entity.Book;
+
+public class BookBeanTest {
+	private static final String TITLE = "Some book";
+
+	BookBean bookBean = new BookBean();
+
+	@Test
+	public void shouldCreateBook() throws Exception {
+		Book book = bookBean.create(TITLE);
+		assertThat(book, notNullValue());
+		assertThat(book.getTitle(), equalTo(TITLE));
+		assertThat(book.getId(), not(isEmptyOrNullString()));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailCreateBookWithNoTitleGiven() throws Exception {
+		bookBean.create(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailCreateBookWithTooShortTitleGiven() throws Exception {
+		bookBean.create("ab");
+	}
+}
