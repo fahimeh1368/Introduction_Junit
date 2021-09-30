3e967d93e763c4df8a192b99fdfd0ebf96049aad
Fahimeh Hajari <fahime.hajari@gmail.com>
2021-09-30 12:28:19 -0400

    Mockito sample added.

---
+++ mockito-example/src/test/java/com/soen345/mockito/ListTestTest.java
@@ -0,0 +1,42 @@
+package com.soen345.mockito;
+
+import static org.junit.Assert.*;
+
+
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ListTestTest {
+
+	@Test
+	public void letsMockListSize() {
+		
+	}
+
+	@Test
+	public void letsMockListSizeWithMultipleReturnValues() {
+	
+	}
+
+	@Test
+	public void letsMockListGet() {
+		
+	}
+
+	@Test
+	public void letsMockListGetWithAny() {
+		
+	}
+}
