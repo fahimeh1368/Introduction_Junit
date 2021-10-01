e4002a7ba3e9baebef4beb498f18f74e50275795
Fahimeh Hajari <fahime.hajari@gmail.com>
2021-10-01 11:05:35 -0400

    Added code of mocking that implemented in class

--- mockito-example/src/test/java/com/soen345/mockito/ListTestTest.java
+++ mockito-example/src/test/java/com/soen345/mockito/ListTestTest.java
@@ -1,10 +1,6 @@
 package com.soen345.mockito;
 
 import static org.junit.Assert.*;
-
-
-
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -22,21 +18,31 @@ public class ListTestTest {
 
 	@Test
 	public void letsMockListSize() {
-		
+		List list = mock(List.class);
+		when(list.size()).thenReturn(10);
+		assertEquals(10,list.size());
 	}
 
 	@Test
 	public void letsMockListSizeWithMultipleReturnValues() {
-	
+		List list = mock(List.class);
+		when(list.size()).thenReturn(10,20);
+		assertEquals(10,list.size());
+		assertEquals(20,list.size());
 	}
 
 	@Test
 	public void letsMockListGet() {
-		
+		List<String> list = mock(List.class);
+		when(list.get(0)).thenReturn("0th element");
+		assertEquals("0th element", list.get(0));
 	}
 
 	@Test
 	public void letsMockListGetWithAny() {
-		
+		List<String> list = mock(List.class);
+		Mockito.when(list.get(Mockito.anyInt())).thenReturn("this is an int");
+		assertEquals("this is an int", list.get(3));
+		assertEquals("this is an int", list.get(6));
 	}
 }
